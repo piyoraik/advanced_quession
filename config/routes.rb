ab8f6dd33f74398f368704de75df5c5c1c73e4e3
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-08-13 10:46:42 +0900

    header

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  resources :users
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
