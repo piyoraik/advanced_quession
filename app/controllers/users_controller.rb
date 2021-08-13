eb60a80a428ca467aee42aa9981b764da954cf58
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-08-13 11:55:04 +0900

    アップロードから表示まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update, :show]
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			redirect_to user_path(@user.id)
+		else
+			render :edit
+		end
+	end
+
+	def show
+	end
+
+	private
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :introduction, :image)
+	end
 end
