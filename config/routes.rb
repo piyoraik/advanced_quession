166b645f6cc4246a4227634e1f4ae0a7182d2bad
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-08-13 10:36:16 +0900

    bootstrap導入

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
