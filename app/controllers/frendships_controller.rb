c2937c28d722183e9ffaf15e917b91a93a5a9348
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-08-14 21:41:22 +0900

    rubocopでの自動修正

--- app/controllers/frendships_controller.rb
+++ app/controllers/frendships_controller.rb
@@ -1,17 +1,16 @@
 class FrendshipsController < ApplicationController
-	before_action :set_user, only: %i[create destroy]
+  before_action :set_user, only: %i[create destroy]
 
-	def create
-		follower = current_user.follow(@user.id)
-		redirect_to user_path(@user.id)
-	end
+  def create
+    current_user.follow(@user.id)
+    redirect_to user_path(@user.id)
+  end
 
-	def destroy
-	end
+  def destroy; end
 
-	private
+  private
 
-	def set_user
-		@user = User.find(params[:user_id])
-	end
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
