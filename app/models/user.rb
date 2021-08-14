2d02e63d3784e0775d9470920589a34c1ddf35a0
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-08-15 01:08:25 +0900

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # アソシエーション
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_one_attached :image
 
-  # フォローする側
+  ## フォローする側
   has_many :follower, class_name: "Frendship", foreign_key: "follower_id", dependent: :destroy
   has_many :follower_user, through: :follower, source: :followed
-  # フォローされる側
+  ## フォローされる側
   has_many :followed, class_name: "Frendship", foreign_key: "followed_id", dependent: :destroy
   has_many :followed_user, through: :followed, source: :follower
 
+  # バリデーション
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
+  # メソッド
+  ## 画像表示
   def image_display(size)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/default.png')
@@ -28,14 +32,17 @@ class User < ApplicationRecord
     image.variant(resize: size.to_s)
   end
 
+  ## フォローする
   def follow(user_id)
     follower.create!(followed_id: user_id)
   end
 
+  ## フォローを外す
   def unfollow(user_id)
     follower.find_by(followed_id: user_id).destroy!
   end
 
+  ## フォロー判定
   def following?(user)
     follower_user.include?(user)
   end
